592cfbcde798d3aa784ab8e02d9a0cabd7f2d4e5
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-10-05 21:46:14 +0200

    export deps.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/index.ts";
+export * from "./deps.ts";
 export { openApi } from "./openapi.ts";
 export * from './lib/index.ts'
