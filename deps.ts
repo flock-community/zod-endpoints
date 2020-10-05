febab16d84f4ba60eb9ef2d8d40fd37c240b322c
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-10-05 17:18:11 +0200

    Export in mod.ts

--- deps.ts
+++ deps.ts
@@ -1,9 +1,11 @@
-// export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/index.ts";
-// export { ZodRawShape } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/base.ts";
-// export { errorUtil } from 'https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/helpers/errorUtil.ts';
+export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/index.ts";
+export { ZodUnion, ZodUnionDef } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/union.ts";
+export { ZodTuple, OutputTypeOfTuple } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/tuple.ts";
+export { ZodRawShape } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/base.ts";
+export { errorUtil } from 'https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/helpers/errorUtil.ts';
 
-export * from "../zod/src/index.ts";
-export { ZodRawShape } from "../zod/src/types/base.ts";
-export { ZodUnion, ZodUnionDef } from "../zod/src/types/union.ts";
-export { ZodTuple, OutputTypeOfTuple } from "../zod/src/types/tuple.ts";
-export { errorUtil } from "../zod/src/helpers/errorUtil.ts";
+// export * from "../zod/src/index.ts";
+// export { ZodRawShape } from "../zod/src/types/base.ts";
+// export { ZodUnion, ZodUnionDef } from "../zod/src/types/union.ts";
+// export { ZodTuple, OutputTypeOfTuple } from "../zod/src/types/tuple.ts";
+// export { errorUtil } from "../zod/src/helpers/errorUtil.ts";
--- mod.ts
+++ mod.ts
@@ -1,1 +1,3 @@
+export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/index.ts";
 export { openApi } from "./openapi.ts";
+export * from './lib/index.ts'
