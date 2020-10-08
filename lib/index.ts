f6493ebb367e869726881908564ba9e829bcce91
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-10-08 08:43:20 +0200

    Add API types

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,4 @@ export {Component, component, ComponentType} from "./component.ts";
 export {Integer, integer} from "./integer.ts";
 export {Router, router, route, response} from "./router.ts";
 export * from './domain.ts'
+export * from './api.ts'
