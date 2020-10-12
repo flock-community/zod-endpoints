fca976299375c9e78c6a399d7633397e7ac90ea0
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-10-12 15:02:29 +0200

    Clean deps

--- deps.ts
+++ deps.ts
@@ -9,9 +9,3 @@ export {
 } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/tuple.ts";
 export { ZodRawShape } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/base.ts";
 export { errorUtil } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/helpers/errorUtil.ts";
-
-// export * from "../zod/src/index.ts";
-// export { ZodRawShape } from "../zod/src/types/base.ts";
-// export { ZodUnion, ZodUnionDef } from "../zod/src/types/union.ts";
-// export { ZodTuple, OutputTypeOfTuple } from "../zod/src/types/tuple.ts";
-// export { errorUtil } from "../zod/src/helpers/errorUtil.ts";
