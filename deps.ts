01f8de41d1d595811b5996733ebefdd77169acac
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-11-06 10:50:16 +0100

    Denoify zod-router

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/index.ts";
+export * from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/deno_lib/index.ts";
 export {
   ZodUnion,
   ZodUnionDef,
-} from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/union.ts";
+} from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/deno_lib/types/union.ts";
 export {
   ZodTuple,
   OutputTypeOfTuple,
-} from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/tuple.ts";
-export { ZodRawShape } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/types/base.ts";
-export { errorUtil } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/src/helpers/errorUtil.ts";
+} from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/deno_lib/types/tuple.ts";
+export { ZodRawShape } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/deno_lib/types/base.ts";
+export { errorUtil } from "https://raw.githubusercontent.com/flock-community/zod/v2_deno/deno_lib/helpers/errorUtil.ts";
