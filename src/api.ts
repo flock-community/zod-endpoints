d757eead09066399352b145d90d0748dc2e26c15
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-11-23 16:13:21 +0100

    Simplify Api Type

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,27 @@
 import * as z from "./deps";
 import {HttpSchema} from "./model";
 
-export type ApiEndpointNames<T extends HttpSchema> = z.output<T> extends {
+export type ApiNames<T extends HttpSchema> = z.output<T> extends {
   name: string;
 }
   ? z.output<T>["name"]
   : never;
-export type ApiRequest<T extends HttpSchema, Key> = Pick<
-  Extract<z.output<T>, { name: Key }>,
+export type ApiRequest<T extends HttpSchema> = Pick<
+  Extract<z.output<T>, { name: ApiNames<T> }>,
   "method" | "path" |  "query" | "headers" | "body"
 >;
-export type ApiResponse<T extends HttpSchema, Key> = Pick<
-  Extract<z.output<T>, { name: Key }>["responses"],
+export type ApiResponse<T extends HttpSchema> = Pick<
+  Extract<z.output<T>, { name: ApiNames<T> }>["responses"],
   "status" | "headers" | "body"
 >;
-export type ApiRouteFunction<T extends HttpSchema, Key> = (
-  request: ApiRequest<T, Key>
-) => Promise<ApiResponse<T, Key>>;
+export type ApiFunction<T extends HttpSchema> = (
+  request: ApiRequest<T>
+) => Promise<ApiResponse<T>>;
 
 export type Api<T extends HttpSchema> = {
-  [Key in ApiEndpointNames<T>]: ApiRouteFunction<T, Key>;
+  [Key in ApiNames<T>]: ApiFunction<T>;
 };
 export type ApiFragment<
   T extends HttpSchema,
-  Key extends ApiEndpointNames<T>
+  Key extends ApiNames<T>
 > = Pick<Api<T>, Key>;
