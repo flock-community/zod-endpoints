39716528e066b67834360f67092121ebe2e9c17e
Willem Veelenturf <willem.veelenturf@gmail.com>
2021-01-26 21:16:12 +0100

    Add response match

--- src/match.ts
+++ src/match.ts
@@ -1,9 +1,24 @@
 import * as z from "./index";
 
 export type MatchRequest = Pick<z.output<z.HttpRequestObject>, "method" | "path" | "query" | "headers" | "body">
+export type MatchResponse = Pick<z.output<z.HttpResponseObject>, "status" | "headers" | "body">
 
-const picker = { method: true, path: true, query:true, headers:true, body: true} as const
-export function matchRequest<T extends z.HttpUnion>(schema: T, req: MatchRequest): z.HttpObject | undefined {
+const requestPicker = { method: true, path: true, query:true, headers:true, body: true} as const
+const responsePicker = { status: true, headers:true, body: true} as const
+
+export function matchRequest(schema: z.HttpUnion, req: MatchRequest): z.HttpObject | undefined {
     return schema.options
-        .find(endpoint => endpoint.pick(picker).check(req))
+        .find(request => request.pick(requestPicker).check(req))
+}
+
+export function matchResponse(responses: z.HttpResponseUnion, req: MatchResponse): z.HttpResponseObject | undefined {
+    if ("shape" in responses) {
+        return responses;
+    }
+    if ("options" in responses) {
+        return responses.options
+            .find(response => response.pick(responsePicker).check(req))
+    }
+    return undefined
+
 }
